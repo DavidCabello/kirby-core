ec66eed8208eaf0aa307eb3839264e28aad39711
David Cabello <davidcabello@MacBook-Pro-de-David.local>
2019-10-28 09:39:16 -0600

    added default templates and waypoints plugin

---
+++ site/templates/default.php
@@ -0,0 +1,12 @@
+<?php snippet('header') ?>
+<?php snippet('navigation') ?>
+
+	<main>
+
+		<h1><?= $page->title()->html() ?></h1>
+			
+
+	</main>
+
+<?php snippet('footer') ?>
+<?php snippet('footer.code') ?>
